c731b550aadb4cc7412384fe6e38cbc89bb17ecc
Habibullah <safana.bib@gmail.com>
2026-07-14 07:43:56 +0700

    revisi session

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,5 +25,15 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (\Symfony\Component\HttpKernel\Exception\HttpException $e, \Illuminate\Http\Request $request) {
+            if ($e->getStatusCode() === 419) {
+                if ($request->expectsJson()) {
+                    return response()->json(['message' => 'Sesi kedaluwarsa. Silakan muat ulang halaman.'], 419);
+                }
+
+                return redirect()->back()
+                    ->withInput($request->except('_token', 'password', 'password_confirmation'))
+                    ->with('error', 'Halaman telah kedaluwarsa karena dibiarkan terbuka terlalu lama. Silakan coba lagi.');
+            }
+        });
     })->create();
